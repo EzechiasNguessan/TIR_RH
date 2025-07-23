5ecb423abf905a9e9d3771e773a45c653ce94418
EzechiasNguessan <151138980+EzechiasNguessan@users.noreply.github.com>
2025-07-23 09:46:35 +0000

    feat: ajout des endpoints Employe avec CRUD complet

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,18 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Employe } from 'src/employe/entities/employe.entity/employe.entity';
-import { Permission } from 'src/permission/entities/permission.entity/permission.entity';
-import { Presence } from 'src/presence/entities/presence.entity/presence.entity';
-import { Departement } from 'src/departement/entities/departement.entity';
+import { join } from 'path';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: [Employe, Permission, Presence, Departement],
-  synchronize: false, // Désactiver en prod
-  migrations: ['dist/migrations/*.js'],
-  migrationsRun: true,
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '123456',
+  database: 'RH_DB',
+  entities: [join(__dirname, '..', '', '*.entity{.ts,.js}')],
+  migrations: [join(__dirname, 'migrations', '', '*{.ts,.js}')],
 };
