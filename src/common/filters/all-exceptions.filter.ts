692b3ad65644a1dfc29deae0e3e888742e4c9fa4
EzechiasNguessan <151138980+EzechiasNguessan@users.noreply.github.com>
2025-07-29 16:42:38 +0000

    Ajout des messages d'erreurs

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -39,43 +39,44 @@ export class AllExceptionsFilter implements ExceptionFilter {
         message = res;
         details = res;
       } else {
-        message = res.message ?? message;
-        details = res;
+        if (Array.isArray(res.message)) {
+          message = res.message.join(', ');
+          details = res.message;
+        } else {
+          message = res.message ?? message;
+          details = res.message ?? res.error;
+        }
       }
     }
 
-    // Messages conviviaux selon le code
     switch (status) {
-      case HttpStatus.BAD_REQUEST: // 400
+      case HttpStatus.BAD_REQUEST:
         userMessage =
           'Certaines données sont invalides, veuillez vérifier votre saisie.';
         break;
-      case HttpStatus.UNAUTHORIZED: // 401
+      case HttpStatus.UNAUTHORIZED:
         userMessage =
           'Vous devez être authentifié pour accéder à cette ressource.';
         break;
-      case HttpStatus.PAYMENT_REQUIRED: // 402
+      case HttpStatus.PAYMENT_REQUIRED:
         userMessage =
           'Un paiement est requis pour accéder à cette fonctionnalité.';
         break;
-      case HttpStatus.FORBIDDEN: // 403
+      case HttpStatus.FORBIDDEN:
         userMessage = 'Vous n’avez pas les permissions nécessaires.';
         break;
-      case HttpStatus.NOT_FOUND: // 404
+      case HttpStatus.NOT_FOUND:
         userMessage = 'La ressource demandée est introuvable.';
         break;
-      case HttpStatus.INTERNAL_SERVER_ERROR: // 500
+      case HttpStatus.INTERNAL_SERVER_ERROR:
         userMessage =
           'Une erreur technique est survenue, veuillez réessayer plus tard.';
-        break;
-      default:
-        userMessage = 'Une erreur est survenue.';
         break;
     }
 
     const errorResponse: ErrorResponse = {
       statusCode: status,
-      message: typeof message === 'string' ? message : 'Erreur',
+      message,
       userMessage,
       timestamp: new Date().toISOString(),
       path: request.url,
